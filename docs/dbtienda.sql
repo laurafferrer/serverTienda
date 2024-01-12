b8a9a857100781ad26ae110e0dfdbeb6275de066
Laura <lferrere.ausiasmarch@gmail.com>
2024-01-12 12:43:47 +0100

    Base de datos y puerto añadidos. Modificado archivo properties.

---
+++ docs/dbtienda.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: database:3306
+-- Tiempo de generación: 12-01-2024 a las 11:34:10
+-- Versión del servidor: 8.1.0
+-- Versión de PHP: 8.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE `dbtienda`;
+USE DATABASE `dbtienda`;
+--
+-- Base de datos: `dbtienda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` bigint NOT NULL,
+  `idProduct` bigint NOT NULL,
+  `idUser` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `category`
+--
+
+CREATE TABLE `category` (
+  `id` bigint NOT NULL,
+  `category` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ordering`
+--
+
+CREATE TABLE `ordering` (
+  `id` bigint NOT NULL,
+  `dateOrder` date NOT NULL,
+  `idUser` bigint NOT NULL,
+  `idPurchase` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `product`
+--
+
+CREATE TABLE `product` (
+  `id` bigint NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `stock` bigint NOT NULL,
+  `image` longblob NOT NULL,
+  `idCategory` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `purchaseDetail`
+--
+
+CREATE TABLE `purchaseDetail` (
+  `id` bigint NOT NULL,
+  `quantity` int NOT NULL,
+  `price` int NOT NULL,
+  `idProduct` bigint NOT NULL,
+  `idOrdering` bigint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` bigint NOT NULL,
+  `dni` varchar(9) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(512) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `lastName1` varchar(255) NOT NULL,
+  `lastName2` varchar(255) DEFAULT NULL,
+  `bithDate` date NOT NULL,
+  `numberPhone` varchar(9) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `postalCode` varchar(10) NOT NULL,
+  `role` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idProduct` (`idProduct`),
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- Indices de la tabla `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `ordering`
+--
+ALTER TABLE `ordering`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idUser` (`idUser`),
+  ADD KEY `idPurchase` (`idPurchase`);
+
+--
+-- Indices de la tabla `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idCategory` (`idCategory`);
+
+--
+-- Indices de la tabla `purchaseDetail`
+--
+ALTER TABLE `purchaseDetail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idProduct` (`idProduct`),
+  ADD KEY `idOrdering` (`idOrdering`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ordering`
+--
+ALTER TABLE `ordering`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `purchaseDetail`
+--
+ALTER TABLE `purchaseDetail`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`);
+
+--
+-- Filtros para la tabla `ordering`
+--
+ALTER TABLE `ordering`
+  ADD CONSTRAINT `ordering_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `ordering_ibfk_2` FOREIGN KEY (`idPurchase`) REFERENCES `purchaseDetail` (`id`);
+
+--
+-- Filtros para la tabla `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`idCategory`) REFERENCES `category` (`id`);
+
+--
+-- Filtros para la tabla `purchaseDetail`
+--
+ALTER TABLE `purchaseDetail`
+  ADD CONSTRAINT `purchaseDetail_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `purchaseDetail_ibfk_2` FOREIGN KEY (`idOrdering`) REFERENCES `ordering` (`id`),
+  ADD CONSTRAINT `purchaseDetail_ibfk_3` FOREIGN KEY (`idOrdering`) REFERENCES `ordering` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
