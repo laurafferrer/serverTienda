44833a16467cd5ab32ae11a6c3fab792a29af6bb
laurafferrer <126758044+laurafferrer@users.noreply.github.com>
2024-01-12 11:37:06 +0000

    Base del servidor.

---
+++ src/main/java/net/ausiasmarch/serverTienda/ServerTiendaApplication.java
@@ -0,0 +1,13 @@
+package net.ausiasmarch.serverTienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerTiendaApplication.class, args);
+	}
+
+}
