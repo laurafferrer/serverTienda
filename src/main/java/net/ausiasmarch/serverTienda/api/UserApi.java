ae6457a79d1c27ad0e9d4ad453e58a8b1944fd9a
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-14 21:12:46 +0100

    Añadido código.

--- src/main/java/net/ausiasmarch/serverTienda/api/UserApi.java
+++ src/main/java/net/ausiasmarch/serverTienda/api/UserApi.java
@@ -1,9 +1,29 @@
 package net.ausiasmarch.serverTienda.api;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+
+import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.ausiasmarch.serverTienda.entity.UserEntity;
+import net.ausiasmarch.serverTienda.service.UserService;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 @RestController
@@ -12,4 +32,25 @@ public class UserApi {
     
     @Autowired
     UserService oUserService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> get(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(oUserService.get(id));
+    }
+
+    @GetMapping("/byUsername/{username}")
+    public ResponseEntity<UserEntity> get(@PathVariable("username") String username) {
+        return ResponseEntity.ok(oUserService.getByUsername(username));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> create(@RequestBody UserEntity oUserEntity) {
+        return ResponseEntity.ok(oUserService.create(oUserEntity));
+    }
+
+    @PostMapping("/forusers")
+    public ResponseEntity<Long> createForUsers(@RequestBody UserEntity oUserEntity) {
+        return ResponseEntity.ok(oUserService.createForUsers(oUserEntity));
+    }
+
 }
