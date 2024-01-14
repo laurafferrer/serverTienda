c2b03c8a8f2b629995263908595b161826ac2d6c
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-14 20:52:44 +0100

    Modificaciones User Api, falta todo.

--- src/main/java/net/ausiasmarch/serverTienda/api/UserApi.java
+++ src/main/java/net/ausiasmarch/serverTienda/api/UserApi.java
@@ -1,5 +1,15 @@
 package net.ausiasmarch.serverTienda.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/user")
 public class UserApi {
     
+    @Autowired
+    UserService oUserService;
 }
