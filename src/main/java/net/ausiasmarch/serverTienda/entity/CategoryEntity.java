4498e84da1ade07e1d3e7b51aebc29c2b0faa713
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-15 20:43:37 +0100

    Añadidos getters y setters.

--- src/main/java/net/ausiasmarch/serverTienda/entity/CategoryEntity.java
+++ src/main/java/net/ausiasmarch/serverTienda/entity/CategoryEntity.java
@@ -22,4 +22,33 @@ public class CategoryEntity {
     @NotBlank
     @Size(min = 3, max = 255)
     private String category;
+
+    public CategoryEntity() {
+    }
+
+    public CategoryEntity(Long id, String category) {
+        this.id = id;
+        this.category = category;
+    }
+
+    public CategoryEntity(String category) {
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
 }
