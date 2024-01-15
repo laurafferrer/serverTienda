ab69c905eaf59da34de2f8679bcd47e618fb0674
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-15 17:11:17 +0100

    Añadido código a PurchaseDetail Repository.

--- src/main/java/net/ausiasmarch/serverTienda/repository/PurchaseDetailRepository.java
+++ src/main/java/net/ausiasmarch/serverTienda/repository/PurchaseDetailRepository.java
@@ -1,5 +1,25 @@
 package net.ausiasmarch.serverTienda.repository;
 
-public class PurchaseDetailRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import net.ausiasmarch.serverTienda.entity.PurchaseDetailEntity;
+
+public interface PurchaseDetailRepository extends JpaRepository<PurchaseDetailEntity, Long> {
+
+    Optional<PurchaseDetailEntity> findById(Long id);
+
+    @Query(value = "SELECT * FROM purchaseDetail WHERE price LIKE %?1%", nativeQuery = true)
+    Optional<PurchaseDetailEntity> findByPrice(String price);
+
+    @Query(value = "SELECT * FROM purchaseDetail WHERE quantity LIKE %?1%", nativeQuery = true)
+    Optional<PurchaseDetailEntity> findByQuantity(String quantity);
+
+    @Modifying
+    @Query(value = "ALTER TABLE purchaseDetail AUTO_INCREMENT = 1", nativeQuery = true)
+    void resetAutoIncrement();
     
 }
