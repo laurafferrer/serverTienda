875d2cad53bba8f79c960ee630f2bbe1302606e6
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-15 15:56:50 +0100

    Añadido código a Product Repository.

--- src/main/java/net/ausiasmarch/serverTienda/repository/ProductRepository.java
+++ src/main/java/net/ausiasmarch/serverTienda/repository/ProductRepository.java
@@ -14,6 +14,16 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     Page <ProductEntity> findBySize(String size);
 
+    @Query(value = "SELECT * FROM product WHERE name LIKE %?1%", nativeQuery = true)
+    Page <ProductEntity> findByName(String name);
+
+    @Query(value = "SELECT p.*, count c(c.id) FROM product p, category c WHERE p.id = c.idCategory GROUP BY p.id ORDER BY count(p.id) DESC", nativeQuery = true)
+    Page <ProductEntity> findProductByCategoryDesc();
+
+    // LUEGO SE USARÁ PARA QUE CUANTO EL STOCK BAJE DE CIERTO NÚMERO SE HAGA UN PEDIDO AL PROVEEDOR
+    @Query(value = "SELECT * FROM product WHERE stock <= ?1", nativeQuery = true)
+    Page <ProductEntity> findByStock(int stock);
+
     @Modifying
     @Query(value = "ALTER TABLE user AUTO_INCREMENT = 1", nativeQuery = true)
     void resetAutoIncrement();
