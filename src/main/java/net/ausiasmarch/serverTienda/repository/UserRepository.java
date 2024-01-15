8fa6ea0281bb1a502a1868b3787e9954f568c97b
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-15 15:47:43 +0100

    Añadido filtrado por "nombre".

--- src/main/java/net/ausiasmarch/serverTienda/repository/UserRepository.java
+++ src/main/java/net/ausiasmarch/serverTienda/repository/UserRepository.java
@@ -23,6 +23,9 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     @Query(value = "SELECT u.*, count o(o.id) FROM user u, ordering o WHERE u.id = o.idUser GROUP BY u.id ORDER BY count(u.id) DESC, nativeQuery = true")
     Page<UserEntity> findUsersByOrderinDesc(Pageable pageable);
 
+    @Query(value = "SELECT * FROM user WHERE length(?1) >= 3 AND (username LIKE %?1% OR name LIKE %?1% OR lastName1 LIKE %?1% OR lastName2 LIKE %?1% OR email LIKE %?1%)", nativeQuery = true)
+    Page<UserEntity> findByUserByNameOrLastnameContainingIgnoreClase(String searchText, String filter, String filter2, String filter3, Pageable pageable);
+
     @Modifying
     @Query(value = "ALTER TABLE user AUTO_INCREMENT = 1", nativeQuery = true)
     void resetAutoIncrement();
