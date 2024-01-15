4a35f1cf14c0586fef02b8fd4c5ac35059c26e45
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-15 16:19:39 +0100

    Añadido código a Cart Repository.

--- src/main/java/net/ausiasmarch/serverTienda/repository/CartRepository.java
+++ src/main/java/net/ausiasmarch/serverTienda/repository/CartRepository.java
@@ -1,5 +1,30 @@
 package net.ausiasmarch.serverTienda.repository;
 
-public class CartRepository {
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import net.ausiasmarch.serverTienda.entity.CartEntity;
+
+public interface CartRepository extends JpaRepository<CartEntity, Long> {
+
+    Optional<CartEntity> findById(Long id);
+
+    //findCartByUser
+    @Query(value = "SELECT u.*, count c(c.id) FROM cart c, user u WHERE c.id = u.idUser GROUP BY c.id ORDER BY count(c.id) DESC", nativeQuery = true)
+    Page<CartEntity> findCartByUserDesc(Pageable pageable);
+
+    //findCartByProduct
+    @Query(value = "SELECT p.*, count c(c.id) FROM cart c, product p WHERE c.id = p.idProduct GROUP BY c.id ORDER BY count(c.id) DESC", nativeQuery = true)
+    Page<CartEntity> findCartByProductDesc(Pageable pageable);
+
+    @Modifying
+    @Query(value = "ALTER TABLE cart AUTO_INCREMENT = 1", nativeQuery = true)
+    void resetAutoIncrement();
     
 }
--- src/main/java/net/ausiasmarch/serverTienda/repository/ProductRepository.java
+++ src/main/java/net/ausiasmarch/serverTienda/repository/ProductRepository.java
@@ -1,6 +1,9 @@
 package net.ausiasmarch.serverTienda.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,6 +13,8 @@ import net.ausiasmarch.serverTienda.entity.ProductEntity;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
+    Optional <ProductEntity> findById(Long id);
+
     Page <ProductEntity> findByIdCategory(Long id);
 
     Page <ProductEntity> findBySize(String size);
@@ -18,7 +23,7 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     Page <ProductEntity> findByName(String name);
 
     @Query(value = "SELECT p.*, count c(c.id) FROM product p, category c WHERE p.id = c.idCategory GROUP BY p.id ORDER BY count(p.id) DESC", nativeQuery = true)
-    Page <ProductEntity> findProductByCategoryDesc();
+    Page <ProductEntity> findProductByCategoryDesc(Pageable pageable);
 
     // LUEGO SE USARÁ PARA QUE CUANTO EL STOCK BAJE DE CIERTO NÚMERO SE HAGA UN PEDIDO AL PROVEEDOR
     @Query(value = "SELECT * FROM product WHERE stock <= ?1", nativeQuery = true)
