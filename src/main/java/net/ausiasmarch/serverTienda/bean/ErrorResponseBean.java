97b0dc7832ccc86c6022cdde712b6d26d7cbe945
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 19:45:22 +0100

    Error response bean comentado.

--- src/main/java/net/ausiasmarch/serverTienda/bean/ErrorResponseBean.java
+++ src/main/java/net/ausiasmarch/serverTienda/bean/ErrorResponseBean.java
@@ -1,3 +1,6 @@
+/*
+   Bean class representing an error response.
+*/
 package net.ausiasmarch.serverTienda.bean;
 
 import java.util.Date;
@@ -9,6 +12,14 @@ public class ErrorResponseBean {
     private String message;
     private String details;
 
+    /*
+     * Constructor to initialize the ErrorResponseBean with the specified parameters.
+     * 
+     * @param timestamp Timestamp of the error response.
+     * @param status HTTP status of the error.
+     * @param message Error message.
+     * @param details Additional details about the error.
+     */
     public ErrorResponseBean(Date timestamp, String status, String message, String details) {
         this.timestamp = timestamp;
         this.status = status;
@@ -16,34 +27,74 @@ public class ErrorResponseBean {
         this.details = details;
     }
 
+    /*
+     * Get the timestamp of the error response.
+     * 
+     * @return Timestamp as a Date object.
+     */
     public Date getTimestamp() {
         return timestamp;
     }
 
+    /*
+     * Set the timestamp of the error response.
+     * 
+     * @param timestamp Timestamp to be set.
+     */
     public void setTimestamp(Date timestamp) {
         this.timestamp = timestamp;
     }
 
+    /*
+     * Get the HTTP status of the error.
+     * 
+     * @return HTTP status as a string.
+     */
     public String getStatus() {
         return status;
     }
 
+    /*
+     * Set the HTTP status of the error.
+     * 
+     * @param status HTTP status to be set.
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /*
+     * Get the error message.
+     * 
+     * @return Error message as a string.
+     */
     public String getMessage() {
         return message;
     }
 
+    /*
+     * Set the error message.
+     * 
+     * @param message Error message to be set.
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /*
+     * Get additional details about the error.
+     * 
+     * @return Details of the error as a string.
+     */
     public String getDetails() {
         return details;
     }
 
+    /*
+     * Set additional details about the error.
+     * 
+     * @param details Details of the error to be set.
+     */
     public void setDetails(String details) {
         this.details = details;
     }
