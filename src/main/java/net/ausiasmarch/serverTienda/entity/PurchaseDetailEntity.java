e813f68e31fda2c198e012b35078aab1b8661ef8
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 17:22:25 +0100

    Entity purchase detail comentado.

--- src/main/java/net/ausiasmarch/serverTienda/entity/PurchaseDetailEntity.java
+++ src/main/java/net/ausiasmarch/serverTienda/entity/PurchaseDetailEntity.java
@@ -1,3 +1,6 @@
+/*
+   Entity class representing a PurchaseDetail with attributes, constraints, and relationships.
+*/
 package net.ausiasmarch.serverTienda.entity;
 
 import jakarta.persistence.Entity;
@@ -19,7 +22,7 @@ public class PurchaseDetailEntity {
     private Long id;
 
     @NotNull
-    private int quantity;
+    private int amount;
 
     @NotNull
     private Double price;
@@ -32,62 +35,131 @@ public class PurchaseDetailEntity {
     @JoinColumn(name = "ordering_id")
     private OrderingEntity ordering;
 
+    /*
+     * Default constructor.
+     */
     public PurchaseDetailEntity() {
     }
 
-    public PurchaseDetailEntity(Long id, int quantity, Double price, ProductEntity product, OrderingEntity ordering) {
+    /*
+     * Constructor with parameters for full entity initialization.
+     * 
+     * @param id        Purchase detail's ID.
+     * @param amount  Purchase detail's amount.
+     * @param price     Purchase detail's price.
+     * @param product   The product associated with the Purchase detail.
+     * @param ordering  The order associated with the Purchase detail.
+     */
+    public PurchaseDetailEntity(Long id, int amount, Double price, ProductEntity product, OrderingEntity ordering) {
         this.id = id;
-        this.quantity = quantity;
+        this.amount = amount;
         this.price = price;
         this.product = product;
         this.ordering = ordering;
     }
 
-    public PurchaseDetailEntity(int quantity, Double price, ProductEntity product, OrderingEntity ordering) {
-        this.quantity = quantity;
+    /*
+     * Constructor with parameters for partial entity initialization.
+     * 
+     * @param amount    Purchase detail's amount.
+     * @param price     Purchase detail's price.
+     * @param product   The product associated with the Purchase detail.
+     * @param ordering  The order associated with the Purchase detail.
+     */
+    public PurchaseDetailEntity(int amount, Double price, ProductEntity product, OrderingEntity ordering) {
+        this.amount = amount;
         this.price = price;
         this.product = product;
         this.ordering = ordering;
     }
 
+    /*
+     * Get Purchase detail's ID.
+     * 
+     * @return Purchase detail's ID.
+     */
     public Long getId() {
         return id;
     }
 
+    /*
+     * Set Purchase detail's ID.
+     * 
+     * @param id    Purchase detail's ID.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
-    public int quantity() {
-        return quantity;
+    /*
+     * Get Purchase detail's amount.
+     * 
+     * @return Purchase detail's amount.
+     */
+    public int getAmount() {
+        return amount;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    /*
+     * Set Purchase detail's amount.
+     * 
+     * @param amount    Purchase detail's amount.
+     */
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
 
-    public Double price() {
+    /*
+     * Get Purchase detail's price.
+     * 
+     * @return Purchase detail's price.
+     */
+    public Double getPrice() {
         return price;
     }
 
+    /*
+     * Set Purchase detail's price.
+     * 
+     * @param price Purchase detail's price.
+     */
     public void setPrice(Double price) {
         this.price = price;
     }
 
-    public ProductEntity product() {
+    /*
+     * Get product associated with Purchase detail.
+     * 
+     * @return product associates with Purchase detail.
+     */
+    public ProductEntity getProduct() {
         return product;
     }
 
+    /*
+     * Set product associated with Purchase detail.
+     * 
+     * @param product   Product associated with Purchase detail.
+     */
     public void setProduct(ProductEntity product) {
         this.product = product;
     }
 
-    public OrderingEntity ordering() {
+    /*
+     * Get order associated with Purchase detail.
+     * 
+     * @return order associated with Purchase detail.
+     */
+    public OrderingEntity getOrdering() {
         return ordering;
     }
 
+    /*
+     * Set order associated with Purchase detail.
+     * 
+     * @return order associated with Purchase detail.
+     */
     public void setOrdering(OrderingEntity ordering) {
         this.ordering = ordering;
     }
-    
 }
