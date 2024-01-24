f059ae3375ff2f80a58aea0763f5c81132cab8e0
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 20:01:15 +0100

    Captcha repository comentado.

--- src/main/java/net/ausiasmarch/serverTienda/repository/CaptchaRepository.java
+++ src/main/java/net/ausiasmarch/serverTienda/repository/CaptchaRepository.java
@@ -1,3 +1,8 @@
+/*
+   CaptchaRepository interface for managing CaptchaEntity persistence.
+   
+   JpaRepository provides standard CRUD operations and queries for the CaptchaEntity entity.
+*/
 package net.ausiasmarch.serverTienda.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -5,5 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import net.ausiasmarch.serverTienda.entity.CaptchaEntity;
 
 public interface CaptchaRepository extends JpaRepository<CaptchaEntity, Long> {
-    
+    // No additional methods needed as JpaRepository provides standard functionality.
 }
