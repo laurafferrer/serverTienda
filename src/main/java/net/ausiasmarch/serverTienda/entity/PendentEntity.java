1a1cd4df7169f0f5484b815974d0e64f9d5f90ab
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 18:31:06 +0100

    Entity pendent comentado.

--- src/main/java/net/ausiasmarch/serverTienda/entity/PendentEntity.java
+++ src/main/java/net/ausiasmarch/serverTienda/entity/PendentEntity.java
@@ -1,3 +1,7 @@
+/*
+   Entity class representing a Pending Entity with attributes, 
+   constraints, and relationships.
+*/
 package net.ausiasmarch.serverTienda.entity;
 
 import java.time.LocalDateTime;
@@ -11,7 +15,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-/* NO ESTA EN LA BASE DE DATOS */
 @Entity
 @Table(name = "pendent")
 public class PendentEntity {
@@ -28,34 +31,74 @@ public class PendentEntity {
 
     private LocalDateTime timecode;
 
+    /*
+     * Get the ID of the pending entity.
+     * 
+     * @return ID of the pending entity.
+     */
     public Long getId() {
         return id;
     }
 
+    /*
+     * Set the ID of the pending entity.
+     * 
+     * @param id ID of the pending entity.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /*
+     * Get the CaptchaEntity associated with the pending entity.
+     * 
+     * @return CaptchaEntity associated with the pending entity.
+     */
     public CaptchaEntity getCaptcha() {
         return captcha;
     }
 
+    /*
+     * Set the CaptchaEntity associated with the pending entity.
+     * 
+     * @param captcha CaptchaEntity associated with the pending entity.
+     */
     public void setCaptcha(CaptchaEntity captcha) {
         this.captcha = captcha;
     }
 
+    /*
+     * Get the token of the pending entity.
+     * 
+     * @return Token of the pending entity.
+     */
     public String getToken() {
         return token;
     }
 
+    /*
+     * Set the token of the pending entity.
+     * 
+     * @param token Token of the pending entity.
+     */
     public void setToken(String token) {
         this.token = token;
     }
 
+    /*
+     * Get the timecode of the pending entity.
+     * 
+     * @return Timecode of the pending entity.
+     */
     public LocalDateTime getTimecode() {
         return timecode;
     }
 
+    /*
+     * Set the timecode of the pending entity.
+     * 
+     * @param timecode Timecode of the pending entity.
+     */
     public void setTimecode(LocalDateTime timecode) {
         this.timecode = timecode;
     }
