52b4fc44bce9f145e9ddd2b630c9a88d0a94e07b
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 18:46:43 +0100

    Session controller comentado.

--- src/main/java/net/ausiasmarch/serverTienda/api/SessionController.java
+++ src/main/java/net/ausiasmarch/serverTienda/api/SessionController.java
@@ -1,15 +1,12 @@
+/*
+   Controller for managing user sessions.
+   Provides endpoints for pre-login and login with captcha.
+*/
 package net.ausiasmarch.serverTienda.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import net.ausiasmarch.serverTienda.bean.CaptchaBean;
 import net.ausiasmarch.serverTienda.bean.CaptchaResponseBean;
@@ -23,11 +20,22 @@ public class SessionController {
     @Autowired
     SessionService oSessionService;
 
+    /*
+     * Endpoint for pre-login operation, returns a captcha for user validation.
+     * 
+     * @return ResponseEntity with CaptchaResponseBean.
+     */
     @GetMapping("/prelogin")
     public ResponseEntity<CaptchaResponseBean> prelogin() {
         return ResponseEntity.ok(oSessionService.prelogin());
     }
 
+    /*
+     * Endpoint for user login with captcha.
+     * 
+     * @param oCaptchaBean CaptchaBean containing user input.
+     * @return ResponseEntity with a string response, typically a token.
+     */
     @PostMapping("/loginCaptcha")
     public ResponseEntity<String> loginCaptcha(@RequestBody CaptchaBean oCaptchaBean) {
         return ResponseEntity.ok("\"" + oSessionService.loginCaptcha(oCaptchaBean) + "\"");
