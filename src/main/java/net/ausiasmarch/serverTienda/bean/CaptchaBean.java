0ea6b9e2a10f49721c44fd29893a548c39e5685a
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 19:38:38 +0100

    Captcha bean comentado.

--- src/main/java/net/ausiasmarch/serverTienda/bean/CaptchaBean.java
+++ src/main/java/net/ausiasmarch/serverTienda/bean/CaptchaBean.java
@@ -1,3 +1,7 @@
+/*
+   Bean class representing a CAPTCHA entity.
+   It holds fields for username, password, token, and answer.
+*/
 package net.ausiasmarch.serverTienda.bean;
 
 public class CaptchaBean {
@@ -7,34 +11,58 @@ public class CaptchaBean {
     private String token = "";
     private String answer = "";
 
+    /*
+     * Getter method for retrieving the username.
+     */
     public String getUsername() {
         return username;
     }
 
+    /*
+     * Setter method for setting the username.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /*
+     * Getter method for retrieving the password.
+     */
     public String getPassword() {
         return password;
     }
 
+    /*
+     * Setter method for setting the password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /*
+     * Getter method for retrieving the token.
+     */
     public String getToken() {
         return token;
     }
 
+    /*
+     * Setter method for setting the token.
+     */
     public void setToken(String token) {
         this.token = token;
     }
 
+    /*
+     * Getter method for retrieving the answer.
+     */
     public String getAnswer() {
         return answer;
     }
 
+    /*
+     * Setter method for setting the answer.
+     */
     public void setAnswer(String answer) {
         this.answer = answer;
     }
