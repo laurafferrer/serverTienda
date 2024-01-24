f5516da5c1958871b699a5af86e9079dfed48714
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 19:44:19 +0100

    Captcha response bean comentado.

--- src/main/java/net/ausiasmarch/serverTienda/bean/CaptchaResponseBean.java
+++ src/main/java/net/ausiasmarch/serverTienda/bean/CaptchaResponseBean.java
@@ -1,3 +1,6 @@
+/*
+   Bean class representing the response for CAPTCHA generation.
+*/
 package net.ausiasmarch.serverTienda.bean;
 
 public class CaptchaResponseBean {
@@ -5,18 +8,38 @@ public class CaptchaResponseBean {
     private String token;
     private byte[] captchaImage;
 
+    /*
+     * Get the Captcha token.
+     * 
+     * @return Captcha token as a String.
+     */
     public String getToken() {
         return token;
     }
 
+    /*
+     * Set the Captcha token.
+     * 
+     * @param token Captcha token to be set.
+     */
     public void setToken(String token) {
         this.token = token;
     }
 
+    /*
+     * Get the byte array representing the Captcha image.
+     * 
+     * @return Byte array representing the Captcha image.
+     */
     public byte[] getCaptchaImage() {
         return captchaImage;
     }
 
+    /*
+     * Set the byte representing the Captcha image.
+     * 
+     * @param captchaImage Byte array representing the Captcha image to be set.
+     */
     public void setCaptchaImage(byte[] captchaImage) {
         this.captchaImage = captchaImage;
     }
