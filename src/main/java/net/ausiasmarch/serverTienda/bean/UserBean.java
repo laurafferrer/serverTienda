9ede503ff729547c3496348b9d7d8013d5ecc920
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 19:46:27 +0100

    User bean comentado.

--- src/main/java/net/ausiasmarch/serverTienda/bean/UserBean.java
+++ src/main/java/net/ausiasmarch/serverTienda/bean/UserBean.java
@@ -1,3 +1,6 @@
+/*
+   Bean class representing a User for data transfer.
+*/
 package net.ausiasmarch.serverTienda.bean;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -9,18 +12,38 @@ public class UserBean {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    /*
+     * Get the username of the user.
+     * 
+     * @return User's username as a string.
+     */
     public String getUsername() {
         return username;
     }
 
+    /*
+     * Set the username of the user.
+     * 
+     * @param username User's username to be set.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /*
+     * Get the password of the user.
+     * 
+     * @return User's password as a string.
+     */
     public String getPassword() {
         return password;
     }
 
+    /*
+     * Set the password of the user.
+     * 
+     * @param password User's password to be set.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
