2e2c9ab1ca8b7c7c5ff67c8925b73bff2bc6888f
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 18:34:49 +0100

    Entity order comentado.

--- src/main/java/net/ausiasmarch/serverTienda/entity/OrderEntity.java
+++ src/main/java/net/ausiasmarch/serverTienda/entity/OrderEntity.java
@@ -1,3 +1,7 @@
+/*
+   Entity class representing an Order with attributes, 
+   constraints, and relationships.
+*/
 package net.ausiasmarch.serverTienda.entity;
 
 import java.time.LocalDate;
@@ -35,9 +39,21 @@ public class OrderEntity {
     @JoinColumn(name = "user_id")
     private UserEntity user_id;
 
+    /*
+     * Default constructor
+     */
     public OrderEntity() {
     }
 
+    /*
+     * Constructor with parameters for full entity initialization.
+     * 
+     * @param id          Order's ID.
+     * @param date_order  Order's date of order.
+     * @param num_bill    Order's bill number.
+     * @param date_bill   Order's date of bill.
+     * @param user_id     User associated with the order.
+     */
     public OrderEntity(Long id, LocalDate date_order, Long num_bill, LocalDate date_bill, UserEntity user_id) {
         this.id = id;
         this.date_order = date_order;
@@ -46,6 +62,14 @@ public class OrderEntity {
         this.user_id = user_id;
     }
 
+    /*
+     * Constructor with parameters for partial entity initialization.
+     * 
+     * @param date_order  Order's date of order.
+     * @param num_bill    Order's bill number.
+     * @param date_bill   Order's date of bill.
+     * @param user_id     User associated with the order.
+     */
     public OrderEntity(LocalDate date_order,  Long num_bill, LocalDate date_bill, UserEntity user_id) {
         this.date_order = date_order;
         this.num_bill = num_bill;
@@ -53,42 +77,92 @@ public class OrderEntity {
         this.user_id = user_id;
     }
 
+    /*
+     * Get the order's ID.
+     * 
+     * @return Order's ID.
+     */
     public Long getId() {
         return id;
     }
 
+    /*
+     * Set the order's ID.
+     * 
+     * @param id Order's ID.
+     */
     public void setId(Long id) {
         this.id = id;
     }
-    
-    public LocalDate getdate_order() {
+
+    /*
+     * Get the order's date of order.
+     * 
+     * @return Order's date of order.
+     */
+    public LocalDate getDate_order() {
         return date_order;
     }
 
-    public void setdate_order(LocalDate date_order) {
+    /*
+     * Set the order's date of order.
+     * 
+     * @param date_order Order's date of order.
+     */
+    public void setDate_order(LocalDate date_order) {
         this.date_order = date_order;
     }
 
-    public Long getnum_bill() {
+    /*
+     * Get the order's bill number.
+     * 
+     * @return Order's bill number.
+     */
+    public Long getNum_bill() {
         return num_bill;
     }
 
-    public void setnum_bill(Long num_bill) {
+    /*
+     * Set the order's bill number.
+     * 
+     * @param num_bill Order's bill number.
+     */
+    public void setNum_bill(Long num_bill) {
         this.num_bill = num_bill;
     }
 
-    public LocalDate getdate_bill() {
+    /*
+     * Get the order's date of bill.
+     * 
+     * @return Order's date of bill.
+     */
+    public LocalDate getDate_bill() {
         return date_bill;
     }
 
-    public void setdate_bill(LocalDate date_bill) {
+    /*
+     * Set the order's date of bill.
+     * 
+     * @param date_bill Order's date of bill.
+     */
+    public void setDate_bill(LocalDate date_bill) {
         this.date_bill = date_bill;
     }
 
+    /*
+     * Get the user associated with the order.
+     * 
+     * @return User associated with the order.
+     */
     public UserEntity getUser() {
         return user_id;
     }
 
+    /*
+     * Set the user associated with the order.
+     * 
+     * @param user_id User associated with the order.
+     */
     public void setUser(UserEntity user_id) {
         this.user_id = user_id;
     }
