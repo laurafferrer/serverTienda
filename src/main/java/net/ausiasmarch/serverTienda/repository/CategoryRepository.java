67093dbb9327c259f2a9510ac5345b7121003f3a
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 20:03:33 +0100

    Category repository comentado.

--- src/main/java/net/ausiasmarch/serverTienda/repository/CategoryRepository.java
+++ src/main/java/net/ausiasmarch/serverTienda/repository/CategoryRepository.java
@@ -1,3 +1,10 @@
+/*
+   CategoryRepository interface for managing CategoryEntity persistence.
+
+   JpaRepository provides standard CRUD operations and queries for the CategoryEntity entity.
+
+   Additional custom queries for specific operations on the CategoryEntity table.
+*/
 package net.ausiasmarch.serverTienda.repository;
 
 import org.springframework.data.domain.Page;
@@ -11,13 +18,12 @@ import net.ausiasmarch.serverTienda.entity.CategoryEntity;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
-    // Methos to get categories by the quantity products associated in ascending order
+    // Method to get categories by the quantity of associated products in ascending order
     @Query(value = "SELECT * FROM category ORDER BY (SELECT COUNT(*) FROM product WHERE product.category_id = category.id) ASC", nativeQuery = true)
     Page<CategoryEntity> findByQuantityProductsAsc(Pageable oPageable);
 
-    // Method to reset the auto-increment value primary key in the category table
+    // Method to reset the auto-increment value of the primary key in the category table
     @Modifying
     @Query(value = "ALTER TABLE category AUTO_INCREMENT = 1", nativeQuery = true)
     void resetAutoIncrement();
-    
 }
