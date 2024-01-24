a86efae14311597446cefe706f7fa1e9190e48e8
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 19:31:34 +0100

    Api category comentado.

--- src/main/java/net/ausiasmarch/serverTienda/api/CategoryApi.java
+++ src/main/java/net/ausiasmarch/serverTienda/api/CategoryApi.java
@@ -1,3 +1,8 @@
+/*
+   Controller for managing categories.
+   Provides endpoints for retrieving, creating, updating, 
+   and deleting categories.
+*/
 package net.ausiasmarch.serverTienda.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,43 +32,77 @@ public class CategoryApi {
     @Autowired
     CategoryService oCategoryService;
 
-    // Get category by id
+    /*
+     * Endpoint for retrieving a category by id.
+     * 
+     * @param id Long containing the id of the category to be retrieved.
+     * @return ResponseEntity containing the category.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<CategoryEntity> get(@PathVariable("id") Long id) {
         return ResponseEntity.ok(oCategoryService.get(id));
     }
 
-    // Get all categories
+    /*
+     * Endpoint for retrieving all categories.
+     * 
+     * @param oPageable Pageable object for pagination.
+     * @return ResponseEntity containing the categories.
+     */
     @GetMapping("")
     public ResponseEntity<Page<CategoryEntity>> getPage(Pageable oPageable) {
         return ResponseEntity.ok(oCategoryService.getPage(oPageable));
     }
 
-    // Create new category
+    /*
+     * Endpoint for creating a new category.
+     * 
+     * @param oCategoryEntity CategoryEntity object containing the category to be created.
+     * @return ResponseEntity containing the id of the created category.
+     */
     @PostMapping("")
     public ResponseEntity<Long> create(CategoryEntity oCategoryEntity) {
         return ResponseEntity.ok(oCategoryService.create(oCategoryEntity));
     }
 
-    // Update existing category
+    /*
+     * Endpoint for updating an existing category.
+     * 
+     * @param oCategoryEntity CategoryEntity object containing the category to be updated.
+     * @return ResponseEntity containing the updated category.
+     */
     @PutMapping("")
     public ResponseEntity<CategoryEntity> update(CategoryEntity oCategoryEntity) {
         return ResponseEntity.ok(oCategoryService.update(oCategoryEntity));
     }
 
-    // Delete existing category
+    /*
+     * Endpoint for deleting an existing category.
+     * 
+     * @param id Long containing the id of the category to be deleted.
+     * @return ResponseEntity containing the id of the deleted category.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Long> delete(@PathVariable("id") Long id) {
         return ResponseEntity.ok(oCategoryService.delete(id));
     }
 
-    // Remove all categories
+    /*
+     * Endpoint for deleting all existing categories.
+     * 
+     * @return ResponseEntity containing the number of deleted categories.
+     */
     @DeleteMapping("/empty")
     public ResponseEntity<Long> empty() {
         return ResponseEntity.ok(oCategoryService.empty());
     }
 
-    // Get categories by the quantity products associated in ascending order
+    /*
+     * Endpoint for retrieving all categories by the quantity products associated in ascending order.
+     * 
+     * @param oPageable Pageable object for pagination.
+     * @return ResponseEntity containing the categories.
+     */
     @GetMapping("/quantityProductsAsc")
     public ResponseEntity<Page<CategoryEntity>> findByQuantityProductsAsc(Pageable oPageable) {
         return ResponseEntity.ok(oCategoryService.findByQuantityProductsAsc(oPageable));
