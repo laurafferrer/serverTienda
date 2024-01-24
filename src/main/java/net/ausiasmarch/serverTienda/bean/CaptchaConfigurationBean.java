1a32f8d08fc22bc4653744680f22e04eaaca08a2
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-24 19:41:16 +0100

    Captcha configuration bean comentado.

--- src/main/java/net/ausiasmarch/serverTienda/bean/CaptchaConfigurationBean.java
+++ src/main/java/net/ausiasmarch/serverTienda/bean/CaptchaConfigurationBean.java
@@ -1,3 +1,7 @@
+/*
+   Configuration class for setting up CAPTCHA generation using Kaptcha library.
+   It defines a bean method that returns a configured DefaultKaptcha object.
+*/
 package net.ausiasmarch.serverTienda.bean;
 
 import org.springframework.context.annotation.Bean;
@@ -11,8 +15,14 @@ import java.util.Properties;
 @Configuration
 public class CaptchaConfigurationBean {
 
+    /*
+     * Bean method to create and configure a DefaultKaptcha object.
+     * 
+     * @return DefaultKaptcha object configured with specified properties.
+     */
     @Bean
     public DefaultKaptcha DefaultKaptcha() {
+        // Set propierties for Captcha generation
         Properties oProperties = new Properties();
         oProperties.setProperty("kaptcha.border", "yes");
         oProperties.setProperty("kaptcha.border.color", "105,179,90");
@@ -22,9 +32,15 @@ public class CaptchaConfigurationBean {
         oProperties.setProperty("kaptcha.textproducer.font.size", "30");
         oProperties.setProperty("kaptcha.session.key", "captchaCode");
         oProperties.setProperty("kaptcha.textproducer.char.length", "5");
+
+        // Create a Config object with the specified properties.
         Config config = new Config(oProperties);
+
+        // Create a DefaultKaptcha object and set its configuration.
         DefaultKaptcha oDefaultKaptcha = new DefaultKaptcha();
         oDefaultKaptcha.setConfig(config);
+
+        // Return the configured DefaultKaptcha object.
         return oDefaultKaptcha;
     }
     
