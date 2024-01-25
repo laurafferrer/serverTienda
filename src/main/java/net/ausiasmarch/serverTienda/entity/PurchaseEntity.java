f50c1ebdb47ad4cbdd7bce334547bdaa32af24a0
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-25 18:06:59 +0100

    Cambio de order a purchase.

--- src/main/java/net/ausiasmarch/serverTienda/entity/PurchaseEntity.java
+++ src/main/java/net/ausiasmarch/serverTienda/entity/PurchaseEntity.java
@@ -1,5 +1,5 @@
 /*
-   Entity class representing an Order with attributes, 
+   Entity class representing an Purchase with attributes, 
    constraints, and relationships.
 */
 package net.ausiasmarch.serverTienda.entity;
@@ -24,7 +24,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "order")
+@Table(name = "purchase")
 public class PurchaseEntity {
 
     @Id
@@ -40,13 +40,13 @@ public class PurchaseEntity {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate date_bill;
 
-    @JsonBackReference("user-order")
+    @JsonBackReference("user-purchase")
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "user_id")
     private UserEntity user_id;
 
-    @JsonManagedReference("order-purchasedetail")
-    @OneToMany(mappedBy = "order", fetch = jakarta.persistence.FetchType.LAZY)
+    @JsonManagedReference("purchase-purchasedetail")
+    @OneToMany(mappedBy = "purchase", fetch = jakarta.persistence.FetchType.LAZY)
     private List<PurchaseDetailEntity> purchasesdetails;
 
     /*
@@ -58,10 +58,10 @@ public class PurchaseEntity {
     /*
      * Constructor with parameters for full entity initialization.
      * 
-     * @param id          Order's ID.
-     * @param date_order  Order's date of order.
-     * @param num_bill    Order's bill number.
-     * @param date_bill   Order's date of bill.
+     * @param id          Purchase's ID.
+     * @param date_order  Purchase's date of order.
+     * @param num_bill    Purchase's bill number.
+     * @param date_bill   Purchase's date of bill.
      * @param user_id     User associated with the order.
      */
     public PurchaseEntity(Long id, LocalDate date_order, Long num_bill, LocalDate date_bill, UserEntity user_id) {
@@ -75,9 +75,9 @@ public class PurchaseEntity {
     /*
      * Constructor with parameters for partial entity initialization.
      * 
-     * @param date_order  Order's date of order.
-     * @param num_bill    Order's bill number.
-     * @param date_bill   Order's date of bill.
+     * @param date_order  Purchase's date of order.
+     * @param num_bill    Purchase's bill number.
+     * @param date_bill   Purchase's date of bill.
      * @param user_id     User associated with the order.
      */
     public PurchaseEntity(LocalDate date_order,  Long num_bill, LocalDate date_bill, UserEntity user_id) {
@@ -88,90 +88,90 @@ public class PurchaseEntity {
     }
 
     /*
-     * Get the order's ID.
+     * Get the Purchase's ID.
      * 
-     * @return Order's ID.
+     * @return Purchase's ID.
      */
     public Long getId() {
         return id;
     }
 
     /*
-     * Set the order's ID.
+     * Set the Purchase's ID.
      * 
-     * @param id Order's ID.
+     * @param id Purchase's ID.
      */
     public void setId(Long id) {
         this.id = id;
     }
 
     /*
-     * Get the order's date of order.
+     * Get the Purchase's date of order.
      * 
-     * @return Order's date of order.
+     * @return Purchase's date of order.
      */
     public LocalDate getDate_order() {
         return date_order;
     }
 
     /*
-     * Set the order's date of order.
+     * Set the Purchase's date of order.
      * 
-     * @param date_order Order's date of order.
+     * @param date_order Purchase's date of order.
      */
     public void setDate_order(LocalDate date_order) {
         this.date_order = date_order;
     }
 
     /*
-     * Get the order's bill number.
+     * Get the Purchase's bill number.
      * 
-     * @return Order's bill number.
+     * @return Purchase's bill number.
      */
     public Long getNum_bill() {
         return num_bill;
     }
 
     /*
-     * Set the order's bill number.
+     * Set the Purchase's bill number.
      * 
-     * @param num_bill Order's bill number.
+     * @param num_bill Purchase's bill number.
      */
     public void setNum_bill(Long num_bill) {
         this.num_bill = num_bill;
     }
 
     /*
-     * Get the order's date of bill.
+     * Get the Purchase's date of bill.
      * 
-     * @return Order's date of bill.
+     * @return Purchase's date of bill.
      */
     public LocalDate getDate_bill() {
         return date_bill;
     }
 
     /*
-     * Set the order's date of bill.
+     * Set the Purchase's date of bill.
      * 
-     * @param date_bill Order's date of bill.
+     * @param date_bill Purchase's date of bill.
      */
     public void setDate_bill(LocalDate date_bill) {
         this.date_bill = date_bill;
     }
 
     /*
-     * Get the user associated with the order.
+     * Get the user associated with the Purchase.
      * 
-     * @return User associated with the order.
+     * @return User associated with the Purchase.
      */
     public UserEntity getUser() {
         return user_id;
     }
 
     /*
-     * Set the user associated with the order.
+     * Set the user associated with the Purchase.
      * 
-     * @param user_id User associated with the order.
+     * @param user_id User associated with the Purchase.
      */
     public void setUser(UserEntity user_id) {
         this.user_id = user_id;
