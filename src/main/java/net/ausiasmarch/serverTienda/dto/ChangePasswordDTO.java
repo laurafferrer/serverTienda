a98aa088f1b5b8e08e14d6442714ea61553c3def
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2024-01-25 11:34:55 +0100

    Archivos de cambio de contraseña por email ChangePasswordDTO y EmailValuesDTO comentados.

--- src/main/java/net/ausiasmarch/serverTienda/dto/ChangePasswordDTO.java
+++ src/main/java/net/ausiasmarch/serverTienda/dto/ChangePasswordDTO.java
@@ -2,46 +2,105 @@ package net.ausiasmarch.serverTienda.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+/**
+ * Data Transfer Object (DTO) for changing user password.
+ */
 public class ChangePasswordDTO {
+
+    /**
+     * The new password.
+     * 
+     * @return The new password.
+     */
     @NotBlank(message = "Password is required")
     private String password;
 
+    /**
+     * The confirmation of the new password.
+     * 
+     * @return The confirmation of the new password.
+     */
     @NotBlank(message = "Confirm password")
     private String confirmPassword;
 
+    /**
+     * The token associated with the password change request.
+     * 
+     * @return The token associated with the password change request.
+     */
     @NotBlank(message = "tokenPassword is required")
     private String tokenPassword;
 
+    /**
+     * Default constructor for ChangePasswordDTO.
+     */
     public ChangePasswordDTO() {
     }
 
+    /**
+     * Constructor for ChangePasswordDTO with parameters.
+     * 
+     * @param password        The new password.
+     * @param confirmPassword The confirmation of the new password.
+     * @param tokenPassword   The token associated with the password change request.
+     */
     public ChangePasswordDTO(String password, String confirmPassword, String tokenPassword) {
         this.password = password;
         this.confirmPassword = confirmPassword;
         this.tokenPassword = tokenPassword;
     }
 
+    /**
+     * Getter for the new password.
+     * 
+     * @return The new password.
+     */
     public String getPassword() {
         return this.password;
     }
 
+    /**
+     * Setter for the new password.
+     * 
+     * @param strPassword The new password.
+     */
     public void setPassword(String strPassword) {
         this.password = strPassword;
     }
 
+    /**
+     * Getter for the confirmation of the new password.
+     * 
+     * @return The confirmation of the new password.
+     */
     public String getConfirmPassword() {
         return this.confirmPassword;
     }
 
+    /**
+     * Setter for the confirmation of the new password.
+     * 
+     * @param strConfirmPassword The confirmation of the new password.
+     */
     public void setConfirmPassword(String strConfirmPassword) {
         this.confirmPassword = strConfirmPassword;
     }
 
+    /**
+     * Getter for the token associated with the password change request.
+     * 
+     * @return The token associated with the password change request.
+     */
     public String getTokenPassword() {
         return this.tokenPassword;
     }
 
-    public void setTokenPassword(String strtokenPassword) {
-        this.tokenPassword = strtokenPassword;
+    /**
+     * Setter for the token associated with the password change request.
+     * 
+     * @param strTokenPassword The token associated with the password change request.
+     */
+    public void setTokenPassword(String strTokenPassword) {
+        this.tokenPassword = strTokenPassword;
     }
 }
--- src/main/java/net/ausiasmarch/serverTienda/dto/EmailValuesDTO.java
+++ src/main/java/net/ausiasmarch/serverTienda/dto/EmailValuesDTO.java
@@ -1,15 +1,60 @@
 package net.ausiasmarch.serverTienda.dto;
 
+/**
+ * Data Transfer Object (DTO) for storing email-related values.
+ */
 public class EmailValuesDTO {
+
+    /**
+     * Sender's email address.
+     * 
+     * @return The sender's email address.
+     */
     private String mailFrom;
+
+    /**
+     * Recipient's email address.
+     * 
+     * @return The recipient's email address.
+     */
     private String mailTo;
+
+    /**
+     * Email subject.
+     * 
+     * @return The email subject.
+     */
     private String mailSubject;
+
+    /**
+     * User's name associated with the email.
+     * 
+     * @return The user's name.
+     */
     private String userName;
+
+    /**
+     * Token associated with password reset.
+     * 
+     * @return The token for password reset.
+     */
     private String tokenPassword;
 
+    /**
+     * Default constructor for EmailValuesDTO.
+     */
     public EmailValuesDTO() {
     }
 
+    /**
+     * Constructor for EmailValuesDTO with parameters.
+     * 
+     * @param mailFrom      Sender's email address.
+     * @param mailTo        Recipient's email address.
+     * @param mailSubject   Email subject.
+     * @param userName      User's name associated with the email.
+     * @param tokenPassword Token associated with password reset.
+     */
     public EmailValuesDTO(String mailFrom, String mailTo, String mailSubject, String userName, String tokenPassword) {
         this.mailFrom = mailFrom;
         this.mailTo = mailTo;
@@ -18,44 +63,93 @@ public class EmailValuesDTO {
         this.tokenPassword = tokenPassword;
     }
     
+    /**
+     * Getter for the sender's email address.
+     * 
+     * @return The sender's email address.
+     */
     public String getMailFrom() {
         return this.mailFrom;
     }
 
+    /**
+     * Setter for the sender's email address.
+     * 
+     * @param mailFrom The sender's email address.
+     */
     public void setMailFrom(String mailFrom) {
         this.mailFrom = mailFrom;
     }
 
+    /**
+     * Getter for the recipient's email address.
+     * 
+     * @return The recipient's email address.
+     */
     public String getMailTo() {
         return this.mailTo;
     }
 
+    /**
+     * Setter for the recipient's email address.
+     * 
+     * @param mailTo The recipient's email address.
+     */
     public void setMailTo(String mailTo) {
         this.mailTo = mailTo;
     }
 
+    /**
+     * Getter for the email subject.
+     * 
+     * @return The email subject.
+     */
     public String getMailSubject() {
         return this.mailSubject;
     }
 
+    /**
+     * Setter for the email subject.
+     * 
+     * @param mailSubject The email subject.
+     */
     public void setMailSubject(String mailSubject) {
         this.mailSubject = mailSubject;
     }
 
+    /**
+     * Getter for the user's name.
+     * 
+     * @return The user's name.
+     */
     public String getUserName() {
         return this.userName;
     }
 
+    /**
+     * Setter for the user's name.
+     * 
+     * @param userName The user's name.
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * Getter for the token associated with password reset.
+     * 
+     * @return The token for password reset.
+     */
     public String getTokenPassword() {
         return this.tokenPassword;
     }
 
+    /**
+     * Setter for the token associated with password reset.
+     * 
+     * @param tokenPassword The token for password reset.
+     */
     public void setTokenPassword(String tokenPassword) {
         this.tokenPassword = tokenPassword;
     }
-    
 }
