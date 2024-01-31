1ca446eac5c9285ca827bd43f76f1418dede640c
laurafferrer <a073667569h@ausiasmarch.net>
2024-01-31 16:41:18 +0100

    Cambio de imports

--- src/main/java/net/ausiasmarch/serverTienda/api/SessionController.java
+++ src/main/java/net/ausiasmarch/serverTienda/api/SessionController.java
@@ -6,7 +6,13 @@ package net.ausiasmarch.serverTienda.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import net.ausiasmarch.serverTienda.bean.CaptchaBean;
 import net.ausiasmarch.serverTienda.bean.CaptchaResponseBean;
