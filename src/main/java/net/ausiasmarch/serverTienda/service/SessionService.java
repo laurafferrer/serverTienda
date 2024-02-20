d2a4b3a456b1b3c176f2868e973ada71de828cc1
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-20 18:31:27 +0100

    Igual

--- src/main/java/net/ausiasmarch/serverTienda/service/SessionService.java
+++ src/main/java/net/ausiasmarch/serverTienda/service/SessionService.java
@@ -13,6 +13,7 @@ import jakarta.transaction.Transactional;
 import net.ausiasmarch.serverTienda.bean.CaptchaBean;
 import net.ausiasmarch.serverTienda.bean.CaptchaResponseBean;
 import net.ausiasmarch.serverTienda.bean.UserBean;
+
 import net.ausiasmarch.serverTienda.entity.CaptchaEntity;
 import net.ausiasmarch.serverTienda.entity.PendentEntity;
 import net.ausiasmarch.serverTienda.entity.UserEntity;
@@ -24,7 +25,6 @@ import net.ausiasmarch.serverTienda.helper.DataGenerationHelper;
 import net.ausiasmarch.serverTienda.helper.JWTHelper;
 
 import net.ausiasmarch.serverTienda.repository.CaptchaRepository;
-import net.ausiasmarch.serverTienda.repository.CategoryRepository;
 import net.ausiasmarch.serverTienda.repository.PendentRepository;
 import net.ausiasmarch.serverTienda.repository.UserRepository;
 
@@ -33,9 +33,6 @@ public class SessionService {
     
     @Autowired
     UserRepository oUserRepository;
-
-    @Autowired
-    CategoryRepository oCategoryRepository;
 
     @Autowired
     CaptchaService oCaptchaService;
@@ -51,9 +48,7 @@ public class SessionService {
 
     // Method to login without captcha
     public String login(UserBean oUserBean) {
-        String strUsername = oUserBean.getUsername();
-        oUserRepository.findByUsernameAndPassword(strUsername, oUserBean.getPassword())
-                .orElseThrow(() -> new ResourceNotFoundException("User or password incorrect"));
+        oUserRepository.findByUsernameAndPassword(oUserBean.getUsername(), oUserBean.getPassword()).orElseThrow(() -> new ResourceNotFoundException("Usuario o contraseña incorrectos"));
         return JWTHelper.generateJWT(oUserBean.getUsername());
     }
 
@@ -69,19 +64,16 @@ public class SessionService {
     // Methos to get the session user
     public UserEntity getSessionUser() {
         if (this.getSessionUsername() != null) {
-            return oUserRepository.findByUsername(this.getSessionUsername()).orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado"));
+            return oUserRepository.findByUsername(this.getSessionUsername())
+                    .orElseThrow(() -> new ResourceNotFoundException("User not found"));
         } else {
             return null;
         }
     }
 
     // Methos to check if the session is active
-    public Boolean isSessionActive() {
-        if (this.getSessionUsername() != null) {
-            return oUserRepository.findByUsername(this.getSessionUsername()).isPresent();
-        } else {
-            return false;
-        }
+    public boolean isSessionActive() {
+        return this.getSessionUsername() != null && oUserRepository.findByUsername(this.getSessionUsername()).isPresent();
     }
 
     // Methos for role checks and access control
@@ -89,9 +81,9 @@ public class SessionService {
     // Admins y Users.
     public Boolean isAdmin() {
         if (this.getSessionUsername() != null) {
-            UserEntity oUserEntityInSession = oUserRepository.findByUsername(this.getSessionUsername())
+            UserEntity userEntitySession = oUserRepository.findByUsername(this.getSessionUsername())
                     .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-            return Boolean.FALSE.equals(oUserEntityInSession.getRole());
+            return Boolean.TRUE.equals(userEntitySession.getRole());
         } else {
             return false;
         }
@@ -99,9 +91,9 @@ public class SessionService {
 
     public Boolean isUser() {
         if (this.getSessionUsername() != null) {
-            UserEntity oUserEntityInSession = oUserRepository.findByUsername(this.getSessionUsername())
+            UserEntity userEntitySession = oUserRepository.findByUsername(this.getSessionUsername())
                     .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-            return Boolean.TRUE.equals(oUserEntityInSession.getRole());
+            return Boolean.FALSE.equals(userEntitySession.getRole());
         } else {
             return false;
         }
@@ -189,7 +181,7 @@ public class SessionService {
                 LocalDateTime timecode = oPendentEntity.getTimecode();
 
                 // Check if the captcha is still valid (within 120s)
-                if (LocalDateTime.now().isAfter(timecode.plusSeconds(120))) {
+                if (LocalDateTime.now().isAfter(timecode.plusSeconds(180))) {
                     throw new UnauthorizedException("Captcha expired");
                 }
 
