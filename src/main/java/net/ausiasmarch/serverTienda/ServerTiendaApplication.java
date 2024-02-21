16ff8e174964b204ac230aabecd0375bfb8a734f
laurafferrer <a073667569h@ausiasmarch.net>
2024-02-21 18:06:46 +0100

    Servlet

--- src/main/java/net/ausiasmarch/serverTienda/ServerTiendaApplication.java
+++ src/main/java/net/ausiasmarch/serverTienda/ServerTiendaApplication.java
@@ -2,7 +2,9 @@ package net.ausiasmarch.serverTienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class ServerTiendaApplication {
 
